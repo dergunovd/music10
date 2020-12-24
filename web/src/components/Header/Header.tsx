83f0fd04c9cd79205c72787a4577578809889d97
Dmitry Dergunov <dmitry@dergunov.net>
2020-12-24 04:25:10 +0300

    #25 goto start screen when pressing logo

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useCallback, useContext } from 'react';
 import {
   HeaderLogo,
   HeaderNav,
   HeaderTitle,
   StyledHeader,
 } from './Header.styled';
+import { GameContext, Screen, WsContext } from '../../contexts';
 
 export interface HeaderProps extends React.HTMLAttributes<HTMLElement> {
   text: string;
   nav?: Array<React.ReactNodeArray>;
 }
 
-export const Header: React.FC<HeaderProps> = ({ text, nav = [], ...props }) => (
-  <StyledHeader {...props}>
-    <HeaderLogo>
-      Music10 <sup>β</sup>
-      <br />
-      <sup>Design by Qurle</sup>
-    </HeaderLogo>
-    <HeaderTitle>{text}</HeaderTitle>
-    <HeaderNav>{nav}</HeaderNav>
-  </StyledHeader>
-);
+export const Header: React.FC<HeaderProps> = ({ text, nav = [], ...props }) => {
+  const { setScreen, setResult } = useContext(GameContext);
+  const ws = useContext(WsContext);
+
+  const goToStart = useCallback(async () => {
+    setResult({ isEnd: false, progress: [] });
+    setScreen(Screen.PLAYLIST);
+    await ws.reconnect();
+  }, []);
+
+  return (
+    <StyledHeader {...props}>
+      <HeaderLogo onClick={goToStart}>
+        Music10 <sup>β</sup>
+        <br />
+        <sup>Designed by Qurle</sup>
+      </HeaderLogo>
+      <HeaderTitle>{text}</HeaderTitle>
+      <HeaderNav>{nav}</HeaderNav>
+    </StyledHeader>
+  );
+};
